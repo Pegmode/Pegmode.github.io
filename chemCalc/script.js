5b6484fe3d6113b2ecb133dc8088c3d498b2b4e5
Daniel Chu <mr.uncool@gmail.com>
2024-05-10 04:32:46 -0600

    added dumb chem calc

---
+++ chemCalc/script.js
@@ -0,0 +1,42 @@
+//this is a really quick and dirty proof of concept
+var areas = [];
+
+var map = L.map('map').setView([53.5461, -113.4937], 13);//edmonton 53.5461° N, 113.4937° W
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
+
+map.on(L.Draw.Event.CREATED, function (event) {
+    var layer = event.layer;
+    drawnItems.addLayer(layer);
+    if (layer instanceof L.Polygon) {
+        //alert('Polygon area: ' + L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]) + ' square meters');
+        areas.push({
+            'polygon': layer,
+            'area':  L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]),//in sqr meters
+        });
+        let area = L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]) / 10000;
+        let carrierRate = parseFloat(document.getElementById('carrierRateTextbox').value);
+        let herbicideRate = parseFloat(document.getElementById('herbicideRateTextbox').value);
+        let waterVolume = parseFloat(document.getElementById('waterTextbox').value);
+
+        let requiredChemForArea = area * carrierRate;
+        let requiredChemForMixing = (herbicideRate * waterVolume) / carrierRate;
+
+        document.getElementById('soltionForAreaTextbox').value = requiredChemForArea ;
+        document.getElementById('requiredMixingLitersTextbox').value = requiredChemForMixing ;
+        document.getElementById('areaHaTextbox').value = area ;
+
+    }
+});
+
